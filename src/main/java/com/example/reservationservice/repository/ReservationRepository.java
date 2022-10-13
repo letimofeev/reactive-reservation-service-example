d2b83445522eb2013da3ffb12fb062050a688a7c
leonid <kotovpetia@gmail.com>
2022-10-13 23:04:55 +0300

    package added

--- src/main/java/com/example/reservationservice/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.reservationservice.repository;
-
-import com.example.reservationservice.entity.Reservation;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface ReservationRepository extends ReactiveMongoRepository<Reservation, String> {
-}
