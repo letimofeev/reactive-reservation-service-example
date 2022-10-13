e58ce692c1cd8e98fd04d88b716fe7a958e2e0d1
leonid <kotovpetia@gmail.com>
2022-10-13 23:05:49 +0300

    files moved

---
+++ reservation-server/src/main/java/com/example/reservationservice/entity/Reservation.java
@@ -0,0 +1,18 @@
+package com.example.reservationservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Reservation {
+
+    @Id
+    private String id;
+    private String reservationName;
+}
